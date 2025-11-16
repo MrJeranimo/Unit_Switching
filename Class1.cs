d4b2c935524d41aa833752e8ec78310e42d06ca2
Jeremy Harrington <harringtonj6@xavier.edu>
2025-11-16 01:31:44 -0500

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,24 @@
+﻿using StarMap.API;
+
+namespace Unit_Switching
+{
+    public class Unit_Switching : IStarMapMod
+    {
+        public bool ImmediateUnload => false;
+
+        public void OnImmediatLoad()
+        {
+            
+        }
+        
+        public void OnFullyLoaded()
+        {
+            Console.WriteLine("Unit_Switching Has Loaded!");
+        }
+
+        public void Unload()
+        {
+            
+        }
+    }
+}
