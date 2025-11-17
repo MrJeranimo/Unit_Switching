2dffa84edb31bde74b38189443f27abd7edfdca7
Jeremy Harrington <harringtonj6@xavier.edu>
2025-11-17 12:06:50 -0500

    Created  function info view that derives info from the currently controlled vessel.

---
+++ Class2.cs
@@ -0,0 +1,53 @@
+﻿using HarmonyLib;
+using KSA;
+using System;
+using System.Reflection;
+
+namespace Unit_Switching
+{
+
+    [HarmonyPatch]
+    public static class SpeedDataPatch
+    {
+        public static MethodBase TargetMethod()
+        {
+
+            Type vehicleType = AccessTools.TypeByName("KSA.Vehicle");
+
+            if (vehicleType == null)
+            {
+                return null;
+            }
+
+            MethodInfo targetMethod = AccessTools.Method(vehicleType, "UpdateNavballData");
+
+            return targetMethod;
+        }
+
+        [HarmonyPostfix]
+        public static void Postfix(object __instance)
+        {
+            try
+            {
+                Vehicle? currentVehicle = Program.ControlledVehicle;
+                Celestial? nearbyCelestial = Program.GetNearbyCelestial();
+                double currentOrbitalSpeed = 0.0;
+                double currentSurfaceSpeed = 0.0;
+                double currentRadarAltitude = 0.0;
+                double currentBarometricAltitude = 0.0;
+                if (currentVehicle != null)
+                {
+                    currentOrbitalSpeed = currentVehicle.OrbitalSpeed;
+                    currentSurfaceSpeed = currentVehicle.GetSurfaceSpeed();
+                    currentRadarAltitude = currentVehicle.GetRadarAltitude();
+                    currentBarometricAltitude = currentVehicle.GetBarometricAltitude();
+                }
+                GameData.UpdateData(currentOrbitalSpeed, currentSurfaceSpeed, currentRadarAltitude, currentBarometricAltitude, currentVehicle, nearbyCelestial);
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+    }
+}
---
+++ Class3.cs
@@ -0,0 +1,25 @@
+﻿using KSA;
+using System;
+
+namespace Unit_Switching
+{
+
+    public static class GameData
+    {
+        public static double CurrentOrbitalSpeed { get; private set; } = 0.0;
+        public static double CurrentSurfaceSpeed { get; private set; } = 0.0;
+        public static double CurrentRadarAltitude { get; private set; } = 0.0;
+        public static double CurrentBarometricAltitude { get; private set; } = 0.0;
+        public static Vehicle? ControlledVehicle { get; private set; } = null;
+        public static Celestial? NearbyCelestial { get; private set; } = null;
+        public static void UpdateData(double newOrbitalSpeed, double newSurfaceSpeed, double newRadarAltitude, double newBarometricAltitude, Vehicle? newVehicle, Celestial? newCelestial)
+        {
+            CurrentOrbitalSpeed = newOrbitalSpeed;
+            CurrentSurfaceSpeed = newSurfaceSpeed;
+            CurrentRadarAltitude = newRadarAltitude;
+            CurrentBarometricAltitude = newBarometricAltitude;
+            ControlledVehicle = newVehicle;
+            NearbyCelestial = newCelestial;
+        }
+    }
+}
